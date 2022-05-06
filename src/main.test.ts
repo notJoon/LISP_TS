ee22300e18d1d81c277eddb5720e5178a105742e
not_joon <lbj199874@gmail.com>
2022-05-06 15:22:45 +0900

    사픽연산 파싱 및 게산

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,59 +1,316 @@
-import { ParseAtom, toChar, Atom, List, ParseList } from './main';
+import { ParseAtom, toChar, Atom, List, ParseList, isAtom, isList, first, rest, Evaluate } from './main';
 
-test('expect return: array of chars', () => {
-    const input = toChar('abc')
-    const expected = ['a', 'b', 'c']
+describe.skip('Parse', () => {
+        test('expect return: array of chars', () => {
+        const input = toChar('abc')
+        const expected = ['a', 'b', 'c']
 
-    expect(input).toEqual(expected)
+        expect(input).toEqual(expected)
+    })
+
+    test('should return a number of Atom, given a string containing a number', () => {
+        const input = ParseAtom('1')
+        const expected = <Atom> {value: 1}
+
+        expect(expected.value).toBe(input.value)
+    })
 })
 
-test('should return a number of Atom, given a string containing a number', () => {
-    const input = ParseAtom('1')
-    const expected = <Atom> {value: 1}
-
-    expect(expected.value).toBe(input.value)
+describe.skip('PaseAtom', () => {
+    test('should return a symbol of Atom, given a string containing a name', () => {
+        const input = ParseAtom('a')
+        const expected = <Atom> {value: 'a'}
+    
+        expect(expected.value).toBe(input.value)
+    })
+    
+    test('throw error if the given string is invalid', () => {
+        const input = () => {ParseAtom('')}
+        expect(input).toThrowError(Error('invalid symbol'))
+    })
 })
 
-test('should return a symbol of Atom, given a string containing a name', () => {
-    const input = ParseAtom('a')
-    const expected = <Atom> {value: 'a'}
 
-    expect(expected.value).toBe(input.value)
+describe.skip('ParseList', () => {
+    test('throw error if the given string is invalid', () => {
+        const input = () => {ParseList('')}
+        expect(input).toThrowError(Error('Invalid expression'))
+    })
+    
+    test('check open delimeter ( is exist', () => {
+        const input = () => {ParseList("123")}
+        expect(input).toThrowError("ListOpenDelimeter ( is not found")
+    })
+    
+    test('return () for empty List', () => {
+        const input = ParseList('()')
+        const expected = <List> {items: []}
+        expect(input.items.length).toBe(expected.items.length)
+    })
+    
+    test('return a List with one element', () => {
+        const input = ParseList('(1)')
+        const expected = <List> { 
+            items: [<Atom> {value: 1}]
+        }
+        expect(input.items.length).toBe(expected.items.length)
+        expect((<Atom>expected.items[0]).value === ((<Atom>input.items[0]).value)).toBe(true)
+    })
+    
+    test('return 2 elements List', () => {
+        const input = ParseList('(1 a)')
+        const expected = <List> {
+            items: [ <Atom> {value: 1}, <Atom> {value: 'a'} ]
+        }
+        expect(input.items.length).toBe(expected.items.length)
+        expect((<Atom>expected.items[0]).value === (<Atom>input.items[0]).value).toBe(true)
+        expect((<Atom>expected.items[1]).value === ((<Atom>input.items[1]).value)).toBe(true)
+    })
+    
+    test('return list that contain operator', () => {
+        const input = ParseList('(() 1 a)')
+        const expected = <List> {
+            items: [ 
+                <List> {items: []}, 
+                <Atom> {value: 1}, 
+                <Atom> {value: 'a'} 
+            ]
+        }
+        expect(input.items.length).toBe(expected.items.length)
+        expect((<Atom>expected.items[0]).value === (<Atom>input.items[0]).value).toBe(true)
+        expect((<Atom>expected.items[1]).value === ((<Atom>input.items[1]).value)).toBe(true)
+        expect((<Atom>expected.items[2]).value === ((<Atom>input.items[2]).value)).toBe(true)
+    })
+    
+    test('return List, given a string with symbol and numeric Atoms', () => {
+        const input = ParseList('(add 1 2)')
+        const expected = <List> {
+            items: [ 
+                <Atom> {value: 'add'}, 
+                <Atom> {value: 1}, 
+                <Atom> {value: 2} 
+            ]
+        }
+        expect(input.items.length).toBe(expected.items.length)
+        expect((<Atom>expected.items[0]).value === (<Atom>input.items[0]).value).toBe(true)
+        expect((<Atom>expected.items[1]).value === ((<Atom>input.items[1]).value)).toBe(true)
+        expect((<Atom>expected.items[2]).value === ((<Atom>input.items[2]).value)).toBe(true)
+    })
+    
+    test('return List, given operator and numeric Atoms', () => {
+        const input = ParseList('(+ 1 2)')
+        const expected = <List> {
+            items: [
+                <Atom> {value: '+'}, 
+                <Atom> {value: 1}, 
+                <Atom> {value: 2} 
+            ]
+        }   
+        expect(input.items.length).toBe(expected.items.length)
+        expect((<Atom>expected.items[0]).value === (<Atom>input.items[0]).value).toBe(true)
+        expect((<Atom>expected.items[1]).value === ((<Atom>input.items[1]).value)).toBe(true)
+        expect((<Atom>expected.items[2]).value === ((<Atom>input.items[2]).value)).toBe(true)
+    })
+    
+    test('return List, given a string of arithmetic expression', () => {
+        const input = ParseList('(+ 1 2 (* 3 4))')
+        const expected = <List> {
+            items: [
+                <Atom> {value: '+'},
+                <Atom> {value: 1}, 
+                <Atom> {value: 2},
+                <List> {
+                    items: [
+                        <Atom> {value: '*'},
+                        <Atom> {value: 3},
+                        <Atom> {value: 4}
+                    ]
+                }
+            ]
+        }
+        expect(input.items.length).toBe(expected.items.length)
+        expect((<Atom>expected.items[0]).value === (<Atom>input.items[0]).value).toBe(true)
+        expect((<Atom>expected.items[1]).value === ((<Atom>input.items[1]).value)).toBe(true)
+        expect((<Atom>expected.items[2]).value === ((<Atom>input.items[2]).value)).toBe(true)
+    
+        const b = (<List>expected.items[3]).items
+        const c = (<List>input.items[3]).items
+        expect(b.length).toBe(c.length)
+        expect((<Atom>b[0]).value === (<Atom>c[0]).value).toBe(true)
+        expect((<Atom>b[1]).value === (<Atom>c[1]).value).toBe(true)
+        expect((<Atom>b[2]).value === (<Atom>c[2]).value).toBe(true)
+    })
+    
+    test('return List given a deep nested list and arithmetic operator', () => {
+        const input = ParseList('(((+ 1 2)))')
+        const expected = <List> {
+            items: [
+                <List> {
+                    items: [
+                        <List> {
+                            items: [
+                                <Atom> {value: '+'},
+                                <Atom> {value: 1},
+                                <Atom> {value: 2}
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+        expect(input.items.length).toBe(expected.items.length)
+    
+        const b = (<List>input.items[0]).items
+        const c = (<List>expected.items[0]).items
+        expect(b.length).toBe(c.length)
+        //console.log(b, c)
+    
+        const d = (<List>b[0]).items 
+        const e = (<List>c[0]).items
+        //console.log(d, e)
+        expect(d.length).toBe(e.length)
+    })
+    
 })
 
-test('ParseAtom throw error if the given string is invalid', () => {
-    const input = () => {ParseAtom('')}
-    expect(input).toThrowError(Error('invalid symbol'))
+describe.skip('Eval', () => {
+// List -> Evaluator -> Atom
+// Match first element with kown operators
+// recursively evaluate he rest of the list 
+// Apply operator to Atoms 
+// Return the result 
+    test('isAtom: return true. if input is Atom', () => {
+        const input = isAtom(<Atom> {value: 3})
+        expect(input).toBe(true)
+    })
+
+    test('isAtom: return false. if input is List', () => {
+        const input = isAtom(<List> { items: []})
+        expect(input).toBe(false)
+    })
+    
+    test('isList: return true. if input is List', () => {
+        const input = isList(<List> {items: []})
+        expect(input).toBe(true)
+    })
+    
+    test('isList: return false. if input is Atom', () => {
+        const input = isList(<Atom> {value: 3})
+        expect(input).toBe(false)
+    })
 })
 
-test('ParseList: throw error if the given string is invalid', () => {
-    const input = () => {ParseList('')}
-    expect(input).toThrowError(Error('Invalid expression'))
+describe.skip('check List', () => {
+    test('return ist element of a List', () => {
+        const input = first(<List> {
+            items: [ <Atom> {value: 3} ]
+        })
+        const expected = <Atom> {value: 3}
+        expect(input.value).toBe(expected.value)
+    })
+
+    test('throw Error if input list is empty', () => {
+        const input = () => {
+            first(<List> { items: []})
+        }
+        expect(input).toThrowError(Error('List is empty'))
+    })
+
+    test('return the rest of List', () => {
+        const input = rest(<List> {
+            items: [
+                <Atom> {value: 3}
+            ]
+        })
+        const expected = <List> {items: []}
+        expect(input.items.length).toBe(expected.items.length)
+    })
 })
 
-test('ParseList: check open delimeter ( is exist', () => {
-    const input = () => {ParseList("123")}
-    expect(input).toThrowError("ListOpenDelimeter ( is not found")
-})
+describe('Arithmetic Operation', () => {
+    test('+ : calculate arithmetic expression correctly', () => {
+        const s = ParseList('(+ 1 2)')
+        const input = Evaluate(s)
+        const expected = <Atom> {value: 3}
+        expect(input.value).toBe(expected.value)
+    })
 
-test('ParseList: return () for empty List', () => {
-    const input = ParseList('()')
-    const expected = <List> {items: []}
-    expect(input.items.length === expected.items.length).toBe(true)
-})
+    test('+ : return false if input is invalid form', () => {
+        const s = ParseList('(+)')
+        const input = () => {Evaluate(s)}
+        expect(input).toThrowError('Not enought argument to the operation')
+    })
 
-test('ParseList: return a List with one element', () => {
-    const input = ParseList('(1)')
-    const expected = <List> { 
-        items: [<Atom> {value: 1}]
-    }
-    expect(input.items.length === expected.items.length).toBe(true)
-})
+    test('- : calculate arithmetic expression correctly', () => {
+        const s = ParseList('(- 1 2)')
+        const input = Evaluate(s)
+        const expected = <Atom> {value: -1}
+        expect(input.value).toBe(expected.value)
+    })
 
-test('ParseList: return multible numbers List', () => {
-    const input = ParseList('(1 a)')
-    const expected = <List> {
-        items: [<Atom> {value: 1}, <Atom> {value: 'a'}]
-    }
-    expect(input.items.length === expected.items.length).toBe(true)
+    test('- : return false if input is invalid form', () => {
+        const s = ParseList('(-)')
+        const input = () => {Evaluate(s)}
+        expect(input).toThrowError('Not enought argument to the operation')
+    })
+
+    test('* : calculate arithmetic expression correctly', () => {
+        const s = ParseList('(* 1 2)')
+        const input = Evaluate(s)
+        const expected = <Atom> {value: 2}
+        expect(input.value).toBe(expected.value)
+    })
+
+    test('* : return false if input is invalid form', () => {
+        const s = ParseList('(-)')
+        const input = () => {Evaluate(s)}
+        expect(input).toThrowError('Not enought argument to the operation')
+    })
+
+    test('/ : calculate arithmetic expression correctly', () => {
+        const s = ParseList('(/ 4 2)')
+        const input = Evaluate(s)
+        const expected = <Atom> {value: 2}
+        expect(input.value).toBe(expected.value)
+    })
+
+    test('/ : return false if input is invalid form', () => {
+        const s = ParseList('(/)')
+        const input = () => {Evaluate(s)}
+        expect(input).toThrowError('Not enought argument to the operation')
+    })
+
+    test('% : calculate arithmetic expression correctly', () => {
+        const s = ParseList('(% 13 5)')
+        const input = Evaluate(s)
+        const expected = <Atom> {value: 3}
+        expect(input.value).toBe(expected.value)
+    })
+
+    test('% : return false if input is invalid form', () => {
+        const s = ParseList('(%)')
+        const input = () => {Evaluate(s)}
+        expect(input).toThrowError('Not enought argument to the operation')
+    })
+
+    test('calculate nested arithmetic expression correctly', () => {
+        const s = ParseList('(+ 1 2 3)')
+        const input = Evaluate(s)
+        const expected = <Atom> {value: 6}
+        expect(input.value).toBe(expected.value)
+    })
+
+    test('calculate nested arithmetic expression correctly', () => {
+        const s = ParseList('(+ 1 2 3)')
+        const input = Evaluate(s)
+        const expected = <Atom> {value: 6}
+        expect(input.value).toBe(expected.value)
+    })
+
+    test('calculate complex arithmetic expression correctly', () => {
+        const s = ParseList('(+ 1 2 (* 3 (/ 8 2)) 10)')
+        const input = Evaluate(s)
+        const expected = <Atom> {value: 25}
+        expect(input.value).toBe(expected.value)
+    })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { isMinusToken } from "typescript"
+
 // split inputs into chars 
 export function toChar(s: string): string[] {
     return Array.from(s)
@@ -8,8 +10,7 @@ export interface Atom {
     value: number | string
 }
 
-// lexer
-export function ParseAtom(s: string):Atom {
+export function ParseAtom(s: string): Atom {
     s.trim()   
     if (s === '') throw new Error('invalid symbol')
     const result = Number(s)
@@ -20,10 +21,10 @@ export function ParseAtom(s: string):Atom {
 // List is a sequencce of Atoms or Lists
 // e.g. (1 2 (3 4) (5 6 7))
 export interface List {
-    items: (Atom|List)[]
+    items: (Atom | List)[]
 }
 
-const ListOpenDelimiter = '('
+const ListOpenDelimeter = '('
 const ListCloseDelimeter = ')'
 const ListElementsDelimeter = " "
 
@@ -39,42 +40,133 @@ function parseListActual(elements: string[], i: number): [List, number] {
     }
 
     while (i < elements.length) {
-        addAtom()
         if (elements[i] === ListCloseDelimeter) {
+            addAtom()
             return [result, i+1]
         }
 
-        if (elements[i] === ListOpenDelimiter) {
+        if (elements[i] === ListOpenDelimeter) {
             const [k, v] = parseListActual(elements, i+1)
             i = v
             result.items.push(k)
+            continue
         }
 
-        // find start index of Atom
-        if (start == -1 && elements[i] != ListOpenDelimiter) {
-            start = i
-        }
-
+        // find start of Atom
         if (elements[i] === ListElementsDelimeter) {
             addAtom()
             start = -1
+        } else {
+            if (start == -1) {
+                start = i
+            }
         }
-
         i++
     }
     return [result, i]
 }
 
 export function ParseList(s: string): List {
-    if (s.length === 0) throw new Error('Invalid expression')
+    if (s.length == 0) throw new Error('Invalid expression')
     s = s.trim()
-
-    // check grammar error
-    if (s[0] !== ListOpenDelimiter) throw new Error("ListOpenDelimeter ( is not found")
+    if (s[0] !== ListOpenDelimeter) throw new Error("ListOpenDelimeter ( is not found")
     
     const elements = toChar(s)
     const [l, m] = parseListActual(elements, 0)
     return <List>l.items[0]
 }
 
+export function isAtom(o: Atom | List): o is Atom {
+    return (o as Atom).value !== undefined
+}
 
+export function isList(o: Atom | List): o is List {
+    return (o as List).items !== undefined
+}
+
+export function first(l: List): Atom {
+    if (l.items.length === 0) throw Error('List is empty')
+    if (isAtom(l.items[0])) {
+        return l.items[0]
+    } else {
+        throw Error('Expected an Atom at the start of a List')
+    }
+}
+
+export function rest(l: List): List {
+    let result = <List> {items: []}
+
+    if (l.items.length === 0) return result 
+    result.items = l.items.slice(1)
+
+    return result 
+}
+
+type basicArithmeticOperator = (a: Atom, b: Atom) => Atom
+interface FunctionMap {
+    [key: string]: basicArithmeticOperator
+}
+
+type ArithmeticOp = (c: number, d: number) => number 
+const validate = (a: Atom, b: Atom, op: ArithmeticOp): Atom => {
+    if (typeof a.value === 'number' && typeof b.value === 'number') {
+        return <Atom> {value: op(a.value, b.value)}
+    }
+    throw Error('Invalid term in arithmetic operation')
+}
+
+const add = (a: Atom, b: Atom): Atom => {
+    return validate(a, b, (c: number, d: number) => c+d)
+}
+
+const minus = (a: Atom, b: Atom): Atom => {
+    return validate(a, b, (c: number, d: number) => c-d)
+}
+
+const multiply = (a: Atom, b: Atom): Atom => {
+    return validate(a, b, (c: number, d: number) => c*d)
+}
+
+const divide = (a: Atom, b: Atom): Atom => {
+    return validate(a, b, (c: number, d: number) => c/d)
+}
+
+const remain = (a: Atom, b: Atom): Atom => {
+    return validate(a, b, (c: number, d: number) => c%d)
+}
+
+const _builtInFunctionName: FunctionMap = {
+    '+': add,
+    '-': minus,
+    '*': multiply,
+    '/': divide,
+    '%': remain,
+}
+
+export function Evaluate(exp: Atom | List): Atom {
+    // A list should start with a symbol like name of operators 
+    // if Atom -> return itself
+    if (isAtom(exp)) {
+        return exp 
+    } else if (isList(exp)) {
+        // if List -> evaluate -> eval rest
+        const f = first(exp)
+        if(f.value in _builtInFunctionName) {
+            const r = rest(exp)
+            const evaluated = r.items.map(k => Evaluate(k))
+
+            if (evaluated.length === 0) throw Error('Not enought argument to the operation ' + f.value)
+
+            let a = evaluated[0]
+            const func = _builtInFunctionName[f.value]
+
+            for (let i=1; i<evaluated.length; i++){
+                a = func(a, evaluated[i])
+            }
+
+            return a 
+        }
+    }
+
+    throw Error('Unknown evaluation error: ' + exp)
+}
